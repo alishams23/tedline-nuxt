444e19234bcbeb90d60dfad07d206998a341f249
ali <alishams.s1380@gmail.com>
2023-10-17 14:50:07 +0330

    f

--- components/section/FooterComponent.vue
+++ components/section/FooterComponent.vue
@@ -1,19 +1,30 @@
 <template>
     <v-footer color="black" v-if="isAuthenticated != true">
       <v-row justify="center" no-gutters>
-        <v-btn
-          v-for="item in menuItems"
-          :key="item"
-          color="white"
-          variant="text"
-          class="mx-2"
-          rounded="xl"
-          :to="item.path"
-        >
-          {{ item.title }}
-        </v-btn>
-        <v-col class="text-center mt-4" cols="12">
-          {{ new Date().getFullYear() }} — <strong>Tedline</strong>
+       <v-col cols="11"  >
+        <div class="d-flex flex-wrap justify-center">
+          <v-btn
+        v-for="item in menuItems"
+        :key="item"
+        color="white"
+        variant="text"
+        class="mx-2"
+        rounded="xl"
+        :to="item.path"
+      >
+        {{ item.title }}
+      </v-btn>
+        </div>
+      <div class="text-center mt-4" >
+        {{ new Date().getFullYear() }} — <strong>Tedline</strong>
+      </div>
+       </v-col>
+        <v-col cols="1">
+          <div >
+            <a href="https://tedline.org/api/wallet/enamad/" >
+              <v-img src="images/enamad.png" height="50"></v-img>
+            </a>
+           </div>
         </v-col>
       </v-row>
     </v-footer>
